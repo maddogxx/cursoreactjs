8b47c0f38b114cf5bacbc8ee9bb34250913d8c7a
Thiago Carvalho da Silva <thiagocs@recife.pe.gov.br>
2022-08-29 13:50:48 -0300

    16. Componente com Propriedade

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,10 +2,12 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './componentes/basicos/Primeiro'
+import CompParametro from './componentes/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
         <Primeiro/>
+        <CompParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={5.3} />
     </div>,
     document.getElementById('root')
 )
