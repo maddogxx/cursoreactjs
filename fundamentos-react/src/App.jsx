731c58738f2186f3e4c0ecac2fbe9e32ce64b7c9
Thiago Carvalho da Silva <thiagocs@recife.pe.gov.br>
2022-08-29 15:21:39 -0300

    22. Componente Card #01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,15 +4,27 @@ import Primeiro from './componentes/basicos/Primeiro'
 import CompParametro from './componentes/basicos/ComParametro'
 import Fragmento from './componentes/basicos/Fragmento'
 import Aleatorio from "./componentes/basicos/Aleatorio";
+import Card from "./componentes/layout/Card";
 
 
 // Forma reduzida usando arrow function
 export default () => (
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min={-10} max={10} />
-        <Fragmento/>
-        <CompParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={5.3} />
-        <Primeiro/>
+        <Card titulo="#04 - Desafio Número Aleatório">
+            <Aleatorio min={-10} max={10} />
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento/>
+        </Card>
+
+        <Card titulo="#02 - Com Parametros">
+            <CompParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={5.3} />
+        </Card>
+        
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro/>
+        </Card>
     </div>
 );
