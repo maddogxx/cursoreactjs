6f4600cda6c02822477028ce647083f8e0cb4aa7
Thiago Carvalho da Silva <thiagocs@recife.pe.gov.br>
2022-08-29 14:08:11 -0300

    18. React Fragment

---
+++ fundamentos-react/src/componentes/basicos/Fragmento.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Fragmento(props) {
+    // Atalho para <ReactFragment> = <>
+
+    return (
+        <>
+            <h2>Fragmento</h2>
+            <p>Cuidado com esse erro!</p>
+        </>
+    );
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './componentes/basicos/Primeiro'
 import CompParametro from './componentes/basicos/ComParametro'
+import Fragmento from './componentes/basicos/Fragmento'
+
 
 ReactDOM.render(
     <div>
         <Primeiro/>
         <CompParametro titulo="Situação do Aluno" aluno="Pedro Silva" nota={5.3} />
+        <Fragmento/>
     </div>,
     document.getElementById('root')
 )
