858ba4de3e317238014ebf8005f7ea546fdcb1b5
Thiago Carvalho da Silva <thiagocs@recife.pe.gov.br>
2022-08-30 15:46:39 -0300

    35. Componente com Estado

--- fundamentos-react/src/componentes/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/componentes/comunicacao/IndiretaPai.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => {
+    const [ nome, setNome ] = useState('?');
+    const [ idade, setIdade ] = useState(0);
+    const [ nerd, setNerd ] = useState(false);
 
     function fornecerInfo(nome, idade, nerd) {
-        console.log(nome, idade, nerd);
+        setNome(nome);
+        setIdade(idade);
+        setNerd(nerd);
     }
 
     return (
         <div>
-            Pai
+            <span>{nome} </span>
+            <span><strong>{idade}</strong> </span>
+            <span>{nerd ? "NERD" : "ALPHABETA"}</span>
+
             <IndiretaFilho quandoClicar={ fornecerInfo } />
         </div>
     );
