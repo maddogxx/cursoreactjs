f1e09d1296243c0f66b51fac6b6dc18e358315cf
maddogxx <maddogxx@gmail.com>
2022-09-04 11:53:50 -0300

    70. useMemo

--- hooks/src/views/examples/UseMemo.jsx
+++ hooks/src/views/examples/UseMemo.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
+
+function soma(a, b) {
+    const futuro = Date.now() + 2000;
+    while(Date.now() < futuro) {} // Esperar 2s
+    
+    return a + b;
+}
 
 const UseMemo = (props) => {
+    const [n1, setN1] = useState(0);
+    const [n2, setN2] = useState(0);
+    const [n3, setN3] = useState(0);
+
+    const resultado = useMemo(() => soma(n1, n2), [n1, n2]);
+
     return (
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
-                subtitle="Retorna um valor memoizado!"
+                subtitle="Retorna um valor memorizado!"
             />
+
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <input type="number" className="input" value={n1} onChange={e => setN1(parseInt(e.target.value))}/>
+                <input type="number" className="input" value={n2} onChange={e => setN2(parseInt(e.target.value))}/>
+                <input type="number" className="input" value={n3} onChange={e => setN3(parseInt(e.target.value))}/>
+                <span className="text">{resultado}</span>
+            </div>
         </div>
+
+        
     )
 }
 
