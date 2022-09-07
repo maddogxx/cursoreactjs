feac37f047bb1a6417801d9b83a72f2bc7f55b00
maddogxx <maddogxx@gmail.com>
2022-09-06 22:20:54 -0300

    90. CSS Modularizado

---
+++ nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import estilo from "../styles/estiloso.module.css";
+
+export default function Estiloso() {
+    return (
+        <div className={estilo.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    );
+}
