d7d4f5953e972d2b6c920b4c5c0a8bda8fd9ef79
maddogxx <maddogxx@gmail.com>
2022-09-07 15:10:42 -0300

    93. Navegação entre Componentes

--- nextjs/fundamentos/src/pages/estiloso.jsx
+++ nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,9 +1,11 @@
 import estilo from "../styles/estiloso.module.css";
+import Link from "next/link";
 
 export default function Estiloso() {
     return (
         <div className={estilo.roxo}>
             <h1>Estilo usando CSS Módulos</h1>
+            <Link href="/">Voltar</Link>
         </div>
     );
 }
--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Inicio() {
   return (
     <div>
       <h1>Início</h1>
+      <Link href="/estiloso">Texto com Estilo</Link>
     </div>
   );
 }
