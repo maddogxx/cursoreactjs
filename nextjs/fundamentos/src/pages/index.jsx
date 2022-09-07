06de878eba1c7bb375f23bf71da7bceaa2373946
maddogxx <maddogxx@gmail.com>
2022-09-07 16:13:49 -0300

    95. Componente Navegador

--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -1,10 +1,16 @@
-import Link from "next/link";
+import Navegador from "../components/Navegador";
 
 export default function Inicio() {
   return (
-    <div>
-      <h1>Início</h1>
-      <Link href="/estiloso">Texto com Estilo</Link>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: "wrap",
+      height: "100vh"
+    }}>
+      <Navegador destino="/exemplo" texto="Exemplo Componente" cor="crimson"/>
+      <Navegador destino="/estiloso" texto="Texto com estilo"/>
     </div>
   );
 }
