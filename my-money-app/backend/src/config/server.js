8c2567af7414dea53b82cb0ae609488e0f4c3e5a
Thiago Carvalho da Silva <thiagocs@recife.pe.gov.br>
2022-09-26 21:37:46 -0300

    Serviço de Ciclo Pagamento

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,11 +1,11 @@
 const port = 3003;
 
-const { v4: uuidv4 } = require('uuid');
+const prisma = require('@prisma/client');
 const bodyParser = require('body-parser');
 const express = require('express');
 const allowCors = require('./cors');
 
-const CicloPagamentoService = require('../api/ciclopagamento/cicloPagamentoService');
+const clientePrisma = new prisma.PrismaClient();
 
 const servidor = express();
 
@@ -13,23 +13,80 @@ servidor.use(bodyParser.json());
 servidor.use(allowCors);
 
 servidor.get("/ciclos", async (req, res) => {
-    const ciclos = await CicloPagamentoService.consultar();
+    const ciclos = await clientePrisma.cicloPagamento.findMany();
 
     res.status(200).json(ciclos);
 });
 
 servidor.get("/ciclos/:id", async (req, res) => {
     const { id } = req.params;
-    const ciclo = await CicloPagamentoService.consultar(id);
+    const ciclo = await clientePrisma.cicloPagamento.findUnique({
+        where: {
+            id: id
+        }
+    });
 
     res.status(200).json(ciclo);
 });
 
 servidor.post("/ciclos", async (req, res) => {
-    const ciclo = await CicloPagamentoService.salvar(req.body);
+    let {nome, mes, ano} = req.body;
 
-    res.status(201).json(ciclo);
+    const novoCicloPagto = await clientePrisma.cicloPagamento.create({
+        data: {
+            nome: nome,
+            mes: mes,
+            ano: ano
+        },
+    });
+
+    res.status(201).json(novoCicloPagto);
 });
+
+servidor.put("/ciclos/:id", async (req, res) => {
+    const { id } = req.params;
+    let {nome, mes, ano, creditos} = req.body;
+
+    let creditosCriados = [];
+    let creditosAlterados = [];
+    creditos.forEach(c => {
+        const {id, nome, valor} = c;
+
+        if (id) {
+            creditosAlterados.push(c);
+        } else {
+            creditosCriados.push(c);
+        }
+
+    });
+
+    console.log(creditosCriados);
+
+    await clientePrisma.cicloPagamento.update({
+        where: {
+            id: +id
+        },
+        data: {
+            nome: nome,
+            mes: mes,
+            ano: ano
+        }
+    });
+
+
+    const novoCicloPagto = await clientePrisma.cicloPagamento.findUnique({
+        where: {
+            id: +id
+        },
+        include: {
+            creditos: true,
+            debitos: true
+        }
+    }) ;
+
+    res.status(201).json(novoCicloPagto);
+});
+
 
 /*
 servidor.post("/atividade", async (req, res) => {
